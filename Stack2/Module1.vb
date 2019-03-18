a5e0a66b398811b97b3835b4c26c09511b7076d6
Phil Wood <phil@pwood.net>
2019-03-18 07:28:04 +0000

    first version

--- Stack2/Module1.vb
+++ Stack2/Module1.vb
@@ -1,7 +1,80 @@
-﻿Module Module1
+﻿Public Class StringStack
 
-    Sub Main()
+    Private items(3) As String
+    Private top As Integer = -1
+
+    Public Sub Push(newItem As String)
+
+        If IsFull() = False Then
+            top += 1
+            items(top) = newItem
+        Else
+            Console.WriteLine("ERROR: stack is full")
+        End If
 
     End Sub
 
-End Module
+    Public Function Pop() As String
+
+        Dim returnItem As String = ""
+
+        If IsEmpty() = False Then
+            returnItem = items(top)
+            top -= 1
+        Else
+            Console.WriteLine("ERROR: Stack is empty")
+        End If
+
+        Return returnItem
+
+    End Function
+
+    Public Function Peek() As String
+
+        Dim returnItem As String = ""
+
+        If IsEmpty() = False Then
+            returnItem = items(top)
+        Else
+            Console.WriteLine("ERROR: Stack is empty")
+        End If
+
+        Return returnItem
+
+    End Function
+
+    Public Function IsFull() As Boolean
+
+        If top >= items.Length - 1 Then
+            Return True
+        Else
+            Return False
+        End If
+
+    End Function
+
+    Public Function IsEmpty() As Boolean
+
+        If top < 0 Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+
+    Public Sub ShowStack()
+
+        If IsEmpty() Then
+            Console.WriteLine(vbCrLf & "The stack is EMPTY")
+        Else
+            Console.WriteLine(vbCrLf & "The stack contains")
+            For i = top To 0 Step -1
+                Console.WriteLine("{0}: {1}", i, items(i))
+            Next
+        End If
+        Console.WriteLine()
+
+    End Sub
+
+End Class
---
+++ Stack2/StringStack.vb
@@ -0,0 +1,37 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim stack As New StringStack
+        Dim item As String
+        Dim userOption As String
+
+        Do
+            stack.ShowStack()
+
+            Console.WriteLine("The options are:")
+            Console.WriteLine("  Push")
+            Console.WriteLine("  Pop")
+            Console.WriteLine("  Peek")
+            Console.WriteLine("  Exit")
+            Console.Write("enter option: ")
+            userOption = Console.ReadLine.ToLower
+
+            Select Case userOption
+                Case "push"
+                    Console.Write("Enter string to push onto the stack: ")
+                    item = Console.ReadLine
+                    stack.Push(item)
+                Case "pop"
+                    item = stack.Pop
+                    Console.WriteLine("{0} has been removed from the stack", item)
+                Case "peek"
+                    item = stack.Peek
+                    Console.WriteLine("The item on the top of the stack is {0}", item)
+            End Select
+
+        Loop Until userOption = "exit"
+
+    End Sub
+
+End Module
